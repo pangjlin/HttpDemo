9944ad06bb3212ce607674aa7a3b4e4e18bc8646
PANGJIANLIN <PANGJIANLIN@kingsoft.com>
2016-04-05 15:42:33 +0800

    delete useless packages

--- src/com/demo/HttpClientDemo.java
+++ src/com/demo/HttpClientDemo.java
@@ -1,13 +1,11 @@
 package com.demo;
 
 import java.io.IOException;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 public class HttpClientDemo {
 	public static void main(String[] args) throws IOException{
